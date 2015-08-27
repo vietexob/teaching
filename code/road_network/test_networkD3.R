7272cb90e75cefb5b55c4718f3c0e7c9bf54d353
Truc Viet Le <vietexob@gmail.com>
2015-08-27 18:49:28 +0800

    To experiment with shortest paths

--- code/road_network/test_networkD3.R
+++ code/road_network/test_networkD3.R
@@ -4,19 +4,23 @@ library(networkD3)
 library(magrittr)
 
 ## Read the segment speed file
-segment.speed <- read.csv(file = './data/traffic/pgh_test_segment_speed_august.csv',
+segment.speed <- read.csv(file = './data/traffic/pgh_train_segment_speed_august.csv',
                           stringsAsFactors = FALSE)
+# segment.speed <- read.csv(file = './data/traffic/pgh_test_segment_speed_august.csv',
+#                           stringsAsFactors = FALSE)
+# segment.speed <- read.csv(file = './data/traffic/was_train_segment_speed_august.csv',
+#                           stringsAsFactors = FALSE)
 
 ## Select only the top few percentage road segmnets to visualize
 ## Otherwise, it becomes too big to be displayed
-top <- 0.05
+top <- 1
 num.segments <- round(top * nrow(segment.speed))
 src <- segment.speed$from[1:num.segments]
 target <- segment.speed$to[1:num.segments]
-# network.data <- data.frame(src, target)
-# 
-# ## Visualize (and save) the network using D3
-# simpleNetwork(network.data) %>% saveNetwork(file = 'pgh_road_network_train.html')
+network.data <- data.frame(src, target)
+
+## Visualize (and save) the network using D3
+simpleNetwork(network.data) %>% saveNetwork(file = 'pgh_road_network_train.html')
 
 ## TODO: Use the new segment_speed data to visualize the network on a Shiny app!
 
@@ -56,11 +60,34 @@ for(i in 1:num.segments) {
 n.nodes <- max(max(src.nodeNum), max(target.nodeNum))
 
 network.data <- cbind(src = src.nodeNum, target = target.nodeNum,
-                      avg.speed = segment.speed$avg_speed)
+                      avg.speed = round(segment.speed$avg_speed, 2),
+                      street.name = segment.speed$street.name)
+
 ## Create a network object from edgelist
 road.network <- network.edgelist(x = network.data,
                                  g = network.initialize(n.nodes, directed = FALSE),
-                                 ignore.eval = FALSE, names.eval = 'avg.speed')
+                                 ignore.eval = FALSE,
+                                 names.eval = c('avg.speed', 'street.name'))
 
-## Visualize
-render.d3movie(road.network, vertex.cex = 0.25)
+## Visualize the network
+out.filename <- './data/networks/pgh_train_segments.html'
+render.d3movie(road.network, vertex.cex = 0.25, vertex.border = '#FFD700',
+               vertex.lwd = 0.05, edge.col = '#808080', edge.lwd = 0.50,
+               edge.border = '#FFFFFF',
+               edge.tooltip = paste('<b>Name:</b>', (road.network %e% 'street.name'), '<br>',
+                                    '<b>Avg. Speed:</b>', (road.network %e% 'avg.speed')),
+               main = 'PGH (Train) Road Network',
+               launchBrowser=F, filename=out.filename, output.mode='HTML')
+
+## Covert from network to igraph
+library(intergraph)
+library(igraph)
+
+road.graph <- asIgraph(road.network)
+## Give the graph edge weights
+E(road.graph)$weight <- E(road.graph)$avg.speed
+
+## Find the shortest path between two given nodes
+foo <- shortest_paths(road.graph, from = 313, output = 'both')
+
+
