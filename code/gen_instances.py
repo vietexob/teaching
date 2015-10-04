e9231f802cf5b2288c68d7f9dce1060dcc77e4df
Truc Viet Le <vietexob@gmail.com>
2015-10-04 20:49:24 +0800

    Generated instances

--- code/gen_instances.py
+++ code/gen_instances.py
@@ -7,10 +7,11 @@ Generate the learning/test instances for the routing programming assignment.
 '''
 
 from igraph import *
+import random
 
-filename = '../data/networks/sin_road_network.graphml'
+# filename = '../data/networks/sin_road_network.graphml'
 # filename = '../data/networks/pgh_road_network.graphml'
-# filename = '../data/networks/was_road_network.graphml'
+filename = '../data/networks/was_road_network.graphml'
 g = Graph.Read_GraphML(f=filename)
 summary(g)
 
@@ -26,11 +27,42 @@ for comp in comps:
         max_index = index
     index += 1
 print(max_index, max_comp)
+giant = comps[max_index]
 
-N = 100
-K = 100
-out_filename = '../data/instances/sin_train_k_eq_n.txt'
+N = 100 # the number of OD pairs
+K = 100 # the number of taxis
+
+## Generate N random OD pairs
+source_list = []
+target_list = []
+for i in range(N):
+    source = random.choice(giant)
+    while source in source_list:
+        source = random.choice(giant)
+    source_list.append(source)
+    
+    target = random.choice(giant)
+    while target in target_list or source == target:
+        target = random.choice(giant)
+    target_list.append(target)
+
+## Generate K vehicle locations
+loc_list = []
+for i in range(K):
+    loc = random.choice(giant)
+    while loc in source_list or loc in target_list or loc in loc_list:
+        loc = random.choice(giant)
+    loc_list.append(loc)
+
+# out_filename = '../data/instances/sin_train_k_eq_n.txt'
+# out_filename = '../data/instances/pgh_train_k_eq_n.txt'
+out_filename = '../data/instances/was_train_k_eq_n.txt'
 out_file = open(out_filename, 'w')
 out_file.write(str(N) + '\n')
 out_file.write(str(K) + '\n')
+for i in range(N):
+    out_file.write(str(source_list[i]) + ", " + str(target_list[i]) + "\n")
+for i in range(K):
+    out_file.write(str(loc_list[i]) + "\n")
+
 out_file.close()
